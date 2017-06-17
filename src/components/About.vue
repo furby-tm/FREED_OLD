3e11bd6f0f1ae805194676ed6abbb8e3f9b62a65
Tyler Furby <tyler@tylerfurby.com>
2017-06-16 18:41:34 -0600

    Added content to About component

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="about">
     <h1>About</h1>
+    <p>This will be a full <strong>about</strong> page explaining why this particular social network is awesome.</p>
   </div>
 </template>
 
@@ -8,5 +9,10 @@
 </script>
 
 <style scoped>
-
+.about {
+  margin: 50px;
+}
+h1 {
+  font-size: 60px;
+}
 </style>
